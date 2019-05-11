0e02a2b241655382d483ac3d2e64523deea019d1
francisguchie <francisguchie@gmail.com>
2019-05-11 19:07:04 +0000

    Http importation from apache21 jar

--- src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
+++ src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
@@ -27,11 +27,14 @@ import java.net.URL;
 import java.util.Map;
 import java.util.Map.Entry;
 
-
+/*
 import org.apache.commons.httpclient.HttpClient; 
 import org.apache.commons.httpclient.HttpStatus; 
 import org.apache.commons.httpclient.NameValuePair;
-import org.apache.commons.httpclient.methods.PostMethod; 
+import org.apache.commons.httpclient.methods.PostMethod; */
+
+import org.apache.commons.httpclient.jar;
+
 
 
 

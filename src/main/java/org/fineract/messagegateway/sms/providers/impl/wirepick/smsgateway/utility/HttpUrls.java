8a386b975fb61db7e97acfb6ff376e92f165c6e5
Guchie <francisguchie@gmail.com>
2019-06-30 00:33:17 +0000

    Update HttpUrls.java

--- src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
+++ src/main/java/org/fineract/messagegateway/sms/providers/impl/wirepick/smsgateway/utility/HttpUrls.java
@@ -18,7 +18,7 @@
  * under the License.
  */
 
-/* package sms.wirepick.smsgateway.Utility;*/
+
 package org.fineract.messagegateway.sms.providers.impl.wirepick.smsgateway.Utility;
 
 import java.io.StringReader;
@@ -32,10 +32,6 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus; 
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod; 
-
-
-
-/* import sms.wirepick.smsgateway.model.MsgStatus; */
 import org.fineract.messagegateway.sms.providers.impl.wirepick.smsgateway.model.MsgStatus;
 
 public class HttpUrls {
